3ab29c7b13f68afe6546770939c926b80199a020
CarlosChasi13 <120142318+CarlosChasi13@users.noreply.github.com>
2024-03-03 14:30:09 -0500

    Adición de Personal Docente

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [PageController::class, 'getData']);
 
 
 Route::get('generate-docentes-pdf', [App\Http\Controllers\PDFController::class, 'generateDocentesPDF'])->name('generateDocentesPDF');
