4fd8a64e1c29e2318d395b19bdb2794c0a31a5ad
Erik Villarreal <dev_seb_3745@proton.me>
2024-03-04 12:42:13 -0500

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+        laravel(
+            [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            {
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
